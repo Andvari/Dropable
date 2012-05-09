bf5b976091e1be44ab710a5e553e48d6724b4901
Nemo <dedmazai74@mail.ru>
2012-05-09 12:04:25 +0400

    Added Nonintersected Panels

--- src/Dropable.py
+++ src/Dropable.py
@@ -11,6 +11,29 @@ class DropablePanel(wx.Panel):
         super(DropablePanel, self).__init__(*args, **kwargs)
         self.Bind(wx.EVT_MOUSE_EVENTS, self.OnMouseEvents)
         self.fDragging = False
+    
+    def SetPosition(self, pos):
+        fFree = True
+        
+        print pos
+        s_lx = pos[0]
+        s_rx = pos[0] + self.GetSize()[0]
+        s_uy = pos[1]
+        s_dy = pos[1] + self.GetSize()[1]
+        
+        for child in self.GetParent().GetChildren():
+            if (self != child):
+                p_lx = child.GetPosition()[0]
+                p_rx = child.GetPosition()[0] + child.GetSize()[0]
+                p_uy = child.GetPosition()[1]
+                p_dy = child.GetPosition()[1] + child.GetSize()[1]
+                if ((s_rx < p_lx)|(s_lx > p_rx)|(s_dy < p_uy)|(s_uy > p_dy)):
+                    pass
+                else:
+                    fFree = False
+            
+        if (fFree):
+            super(DropablePanel, self).SetPosition(pos)
         
     def OnMouseEvents(self, e):
         abs_coords = e.GetEventObject().GetPosition() + e.GetPosition()
@@ -34,7 +57,8 @@ class DropablePanel(wx.Panel):
                 if (child.fDragging):
                     child.SetPosition((abs_coords[0] - child.GetSize()[0]/2, abs_coords[1] - child.GetSize()[1]/2))
                     break
-
+                
+        e.GetEventObject().GetParent().Refresh()
 
 class MainWindow(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -44,13 +68,13 @@ class MainWindow(wx.Frame):
         
         pnl = DropablePanel(mpnl, wx.ID_ANY, size = (400, 400), pos = (0, 0), style = wx.BORDER_SIMPLE)
         
-        self.dp1 = DropablePanel(pnl, wx.ID_ANY, size = (20, 20), pos = (0, 0), style = wx.BORDER_SIMPLE)
+        self.dp1 = DropablePanel(pnl, wx.ID_ANY, size = (30, 30), pos = (0, 0), style = wx.BORDER_SIMPLE)
         self.dp1.SetBackgroundColour("BLACK")
         
-        self.dp2 = DropablePanel(pnl, wx.ID_ANY, size = (40, 40), pos = (100, 100), style = wx.BORDER_SIMPLE)
+        self.dp2 = DropablePanel(pnl, wx.ID_ANY, size = (30, 30), pos = (100, 100), style = wx.BORDER_SIMPLE)
         self.dp2.SetBackgroundColour("BLUE")
         
-        self.dp3 = DropablePanel(pnl, wx.ID_ANY, size = (60, 60), pos = (100, 100), style = wx.BORDER_SIMPLE)
+        self.dp3 = DropablePanel(pnl, wx.ID_ANY, size = (30, 30), pos = (200, 200), style = wx.BORDER_SIMPLE)
         self.dp3.SetBackgroundColour("RED")
         
         self.Center()
